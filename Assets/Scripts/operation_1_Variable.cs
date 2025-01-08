ec88923017016466ab70bb22951a29221c5ce26a
fgnmk <opfgnmk@gmail.com>
2025-01-08 23:04:43 +0800

    Update 作業1 作業2

---
+++ Assets/Scripts/operation_1_Variable.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class operation_1_Variable : MonoBehaviour {
+
+    public int price = 999;
+    private float jumpHeight = 77.5f;
+    public string weapon = "三項之力";
+    private bool whetherToObtainDragonSoul = true;
+}
