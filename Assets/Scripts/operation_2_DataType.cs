052d753fea5508f58b2f63c86bb61824ee334fc4
fgnmk <opfgnmk@gmail.com>
2025-01-14 09:04:45 +0800

    Update operation_2_DataType.cs

--- Assets/Scripts/operation_2_DataType.cs
+++ Assets/Scripts/operation_2_DataType.cs
@@ -2,9 +2,9 @@ using UnityEngine;
 
 public class operation_2_DataType : MonoBehaviour
 {
-    public float a = 7.7f;
-    public double b = 0.12345f;
-    public double c = 1.23456789f;
+    public float variableFloat = 7.7f;
+    public double variableDouble = 0.1235;
+    public decimal variableFecimal = 1.23456789m;
 
 
 }
